c46e94b52513a12240a92f603e484ade6572c1d7
Julian Bolanos <julian.camilob24@gmail.com>
2021-03-13 01:04:46 -0500

    Implemented methods: getMaxIdx and getSolutions. Able to read the input and build the expectated output for the samples. It was tested with the file test.txt.

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -16,8 +16,7 @@ public class Main {
 		
 		String line = br.readLine();
 		
-		int books;
-		int money;
+		int books, money;
 		int[] prices;
 		
 		while(line != null) {
@@ -26,18 +25,71 @@ public class Main {
 			prices = getPrices(br.readLine());
 			money = Integer.parseInt(br.readLine());
 			
-			bw.write("books: " + books + "\n" + 
-					 "prices: " + Arrays.toString(prices) + "\n" +
-					 "money: " + money + "\n\n");
+			Arrays.sort(prices);
+			
+			int[] solutions = getSolutions(prices, books, money);
 			
 			br.readLine();
 			line = br.readLine();
+			
+			int i = 0;
+			int j = 1;
+			
+			//Output format
+			String output = String.format("Peter should buy books whose prices are %d and %d.\n\n", solutions[i], solutions[j]);
+			bw.write(output);
 		}
 
 		
 		br.close();
 		bw.close();
 	}
+
+
+
+	private static int[] getSolutions(int[] prices, int books, int money) {
+		
+		int i = 0;
+		int j = 1;
+		int maxIdx = getMaxIdx(prices, books, money);
+		int[] values = new int[2];
+		
+		boolean stop = false;
+		int diff = money;
+		
+		for (int k = 0; k < maxIdx && !stop; k++) {
+			
+			int tempPrice = prices[k];
+			if(diff > Math.abs(tempPrice - (money - tempPrice))) {
+				diff = Math.abs(tempPrice - (money - tempPrice));
+				values[i] = tempPrice;
+				values[j] = money - tempPrice;
+			}
+		}
+		
+		return values;
+	}
+
+	private static int getMaxIdx(int[] prices, int books, int money) {
+		
+		int low = 0;
+		int top = books - 1;	
+		int maxIdx = top;
+		
+		while(low < top) {
+			
+			int mid = (top + low)/2;		
+			if (prices[mid] > money) {
+				top = mid;
+			} else {
+				low = mid + 1;
+			}
+			maxIdx = top;
+		}
+		return maxIdx;
+	}
+
+
 
 	private static int[] getPrices(String line) {
 		
