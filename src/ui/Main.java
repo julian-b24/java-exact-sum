31cd6c51953782c536de46ad5e7711e61d177758
Julian Bolanos <julian.camilob24@gmail.com>
2021-03-12 22:11:11 -0500

    Implemented way to read the input file.

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,11 +1,54 @@
 package ui;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String line = br.readLine();
+		
+		int books;
+		int money;
+		int[] prices;
+		
+		while(line != null) {
+			
+			books = Integer.parseInt(line);
+			prices = getPrices(br.readLine());
+			money = Integer.parseInt(br.readLine());
+			
+			bw.write("books: " + books + "\n" + 
+					 "prices: " + Arrays.toString(prices) + "\n" +
+					 "money: " + money + "\n\n");
+			
+			br.readLine();
+			line = br.readLine();
+		}
 
 		
+		br.close();
+		bw.close();
+	}
+
+	private static int[] getPrices(String line) {
 		
+		String[] strings = line.split(" ");
+		int[] nums = new int[strings.length];
+		
+		for (int i = 0; i < strings.length; i++) {
+			nums[i] = Integer.parseInt(strings[i]);
+		}
+		
+		return nums;
 	}
 
 }
